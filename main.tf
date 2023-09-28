aebb8c04f4c6c6fdfb95f5bb4e9fa4cf47823134
joraff <joraff@gmail.com>
2023-09-27 22:34:46 -0500

    Enable multi-region providers

--- main.tf
+++ main.tf
@@ -26,12 +26,12 @@ terraform {
 }
 
 
-provider "aws" {
-  alias  = "us-east-1"
-  region = "us-east-1"
-}
+# provider "aws" {
+#   alias  = "us-east-1"
+#   region = "us-east-1"
+# }
 
-provider "aws" {
-  alias  = "us-east-2"
-  region = "us-east-2"
-}
+# provider "aws" {
+#   alias  = "us-east-2"
+#   region = "us-east-2"
+# }
